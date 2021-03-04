918a4aca23a328dc6a50ec7836ad576ae26d8575
Siobhanmcc <siobhanmccauley@gmail.com>
2021-03-04 15:58:24 +0000

    Add Email.js script

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "template_yzs246c", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
